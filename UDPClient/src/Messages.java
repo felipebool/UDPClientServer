c63e1a86df02dbbe66ae8d104f5fc39b42738da9
felipe <bolzin@gmail.com>
2015-06-03 19:41:24 -0300

    removido o java

--- UDPClient/src/Messages.java
+++
@@ -1,8 +0,0 @@
-
-public final class Messages {
-	public static final int OK = 1;
-	public static final int NOK = 2;
-	public static final int ABORT = 3;
-	public static final int CHANGE = 4;
-	public static final int COMMIT = 5;
-}
