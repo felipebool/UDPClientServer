aad7cc7c976b0c19a1c3d5fe05ad9190092a6405
felipe <bolzin@gmail.com>
2015-06-05 00:46:37 -0300

    funcionando ok para 3 servidores e 3 clientes, ainda falta fazer a parte do dado a ser comitado

--- clients/ClientUDP1.py
+++ clients/ClientUDP1.py
@@ -1,52 +1,57 @@
-from time import sleep
-from random import randint
-
-import socket
 import sys
-
+import socket
+from time import sleep
 sys.path.append('../messages')
 import ProtocolMessages
+from random import randint
 
 class ClientUDP():
     hostname = "localhost"
-    delay = 0
+    okResponse = 0
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    okFromServer1 = False
-    okFromServer2 = False
-    okFromServer3 = False
+    def __init__(self, port1, port2, port3):
+        self.delay = float(randint(5, 8))
 
-    # Construtor da classe, inicializa atributo delay. O delay determina o
-    # tempo que o cliente irá esperar para enviar a mensagem commit para o
-    # servidor, o valor deste atributo varia de 0 a 5 segundos.
-    def __init__(self):
-        self.delay = float(random.randint(0,5))
+        self.serverPort1 = port1
+        self.serverPort2 = port2
+        self.serverPort3 = port3
 
-    def sendMessage(self, message, port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(message + "\n", (self.hostname, port))
-
+    def sendChangeMessage(self, message, port, sock):
+        sock.sendto(message, (self.hostname, port))
         received = sock.recv(1024)
         self.manageMessages(received, sock, port)
 
-    def manageMessages(self, data, socket, port):
-        if (data.rstrip() == ProtocolMessages.Messages.OK):
-            print "Recebi um OK do servidor"
+    def manageMessages(self, data, sock, port):
+        if (data == ProtocolMessages.Messages.OK):
+            print "Recebi um OK do servidor/porta: " + str(port)
+            ClientUDP.okResponse = ClientUDP.okResponse + 1
 
+        elif (data == ProtocolMessages.Messages.NOK):
+            print "Recebi um NOK do servidor/porta: " + str(port) + " -> Enviando ABORT"
+            sock.sendto(ProtocolMessages.Messages.ABORT, (self.hostname, self.serverPort1))
+            sock.sendto(ProtocolMessages.Messages.ABORT, (self.hostname, self.serverPort2))
+            sock.sendto(ProtocolMessages.Messages.ABORT, (self.hostname, self.serverPort3))
+
+
+    def checkOkAnswers(self, sock):
+        if (ClientUDP.okResponse == 3):
+            print "Recebi OK de todos os servidores -> Enviando COMMIT"
             sleep(self.delay)
-            commit_message = ProtocolMessages.Messages.COMMIT + "\n"
-            socket.sendto(commit_message, (self.hostname, port))
-        elif (data.rstrip() == ProtocolMessages.Messages.NOK):
-            print "Recebi um NOK do servidor"
+            sock.sendto(ProtocolMessages.Messages.COMMIT, (self.hostname, self.serverPort1))
+            sock.sendto(ProtocolMessages.Messages.COMMIT, (self.hostname, self.serverPort2))
+            sock.sendto(ProtocolMessages.Messages.COMMIT, (self.hostname, self.serverPort3))
+            ClientUDP.okResponse = 0
 
 if __name__ == "__main__":
-    client1 = ClientUDP()
+    client1 = ClientUDP(9999, 9998, 9997)
 
-    for i in range(0, )
-    #1 SERVER
-	client1.sendMessage(ProtocolMessages.Messages.CHANGE, 9999)
+    for i in range(30):
+        #1 SERVER
+        client1.sendChangeMessage(ProtocolMessages.Messages.CHANGE, 9999, client1.serverSocket)
+        #2 SERVER
+        client1.sendChangeMessage(ProtocolMessages.Messages.CHANGE, 9998, client1.serverSocket)
+        #3 SERVER
+        client1.sendChangeMessage(ProtocolMessages.Messages.CHANGE, 9997, client1.serverSocket)
+        client1.checkOkAnswers(client1.serverSocket)
 
-    #2 SERVER
-    client1.sendMessage(ProtocolMessages.Messages.CHANGE, 9998)
-
-    #3 SERVER
-    client1.sendMessage(ProtocolMessages.Messages.CHANGE, 9997)
--- servers/ServerUDP1.py
+++ servers/ServerUDP1.py
@@ -9,56 +9,44 @@ import ProtocolMessages
 
 class ServerUDP (SocketServer.BaseRequestHandler):
     changeReceived = False
-    pointX = 0
-    pointY = 0
-
-    def __init__(self):
-        self.img = Image.new('RGB', (255, 255), "black")
-        self.pixels = self.img.load()
 
     def handle(self):
-        self.data = self.request[0].strip()
+        self.data = self.request[0]
         socket = self.request[1]
 
-        self.manageMessages(data, socket)
+        self.manageMessages(self.data, socket)
 
     def manageMessages(self, data, socket):
-        print "Recebi um  " + data + " do cliente: " + self.client_address[0]
-
         if (data == ProtocolMessages.Messages.CHANGE):
+            print "recebi um change "
             if (not ServerUDP.changeReceived):
                 ServerUDP.changeReceived = True
-                print "Enviando um " + ProtocolMessages.Messages.OK + " para o cliente"
-                ok_message = ProtocolMessages.Messages.OK + "\n"
-                socket.sendto(ok_message, self.client_address)
-
+                socket.sendto(ProtocolMessages.Messages.OK, self.client_address)
             else:
-                print "Enviando um " + ProtocolMessages.Messages.NOK + " para o cliente"
-                nok_message = ProtocolMessages.Messages.NOK + "\n"
-                socket.sendto(nok_message.upper(), self.client_address)
+                socket.sendto(ProtocolMessages.Messages.NOK, self.client_address)
 
-        elif (data == ProtocolMessages.Messages.COMMIT):
+        elif (self.data == ProtocolMessages.Messages.COMMIT and ServerUDP.changeReceived):
+            print "recebi um commit"
             self.commitDataModification()
             ServerUDP.changeReceived = False
 
-        elif (data == ProtocolMessages.Messages.ABORT):
+        elif (self.data == ProtocolMessages.Messages.ABORT):
+            print "recebi um abort"
             ServerUDP.changeReceived = False
 
+    # A mensagem COMMIT padrao tem valor 5. Quando um servidor recebe uma
+    # mensagem maior do que 5, ele sabe que acabou de receber um commit.
+    # A diferenca entre o valor da mensagem recebida e o valor padrao para
+    # a mensagem commit representa a cor do pixel que sera alterado por
+    # aquele cliente. Desta forma, o valor da cor pega carona com a mensagem
+    # COMMIT.
     def commitDataModification(self):
-        # A mensagem COMMIT padrão tem valor 5. Quando um servidor recebe uma
-        # mensagem maior do que 5, ele sabe que acabou de receber um commit.
-        # A diferença entre o valor da mensagem recebida e o valor padrão para
-        # a mensagem commit representa a cor do pixel que será alterado por
-        # aquele cliente. Desta forma, o valor da cor pega carona com a mensagem
-        # COMMIT.
-        ServerUDP.pixels[ServerUDP.pointX, ServerUDP.pointY] = (ServerUDP.pointX, ServerUDP.pointY, this.data - ProtocolMessages.Messages.COMMIT)
-        ServerUDP.pointX = ServerUDP.pointX + 1
-        ServerUDP.pointY = ServerUDP.pointY + 1
-        print "Entrei no commitDataModification"
+        print "commitDataModification"
+        print str(self.server_address[1])
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", int(sys.argv[1])
+
     server = SocketServer.UDPServer((HOST, PORT), ServerUDP)
     server.serve_forever()
 
-#socket.sendto(data.upper(), self.client_address)
